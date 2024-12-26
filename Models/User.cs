788baedf3865fecfa5dd485aa851a2b52366165d
Kuba Bolek <jakubbolek02@gmail.com>
2024-12-26 11:23:52 +0100

    Added MinLength attribute in Users model and added LoginDto for better readability.

--- Models/User.cs
+++ Models/User.cs
@@ -7,10 +7,12 @@ public class User : IdentityUser
 {
     [PersonalData]
     [Required]
+    [MinLength(3)]
     [MaxLength(255)]
     public string? FirstName { get; set; }
     [PersonalData]
     [Required]
+    [MinLength(3)]
     [MaxLength(255)]
     public string? LastName { get; set; }
 }
